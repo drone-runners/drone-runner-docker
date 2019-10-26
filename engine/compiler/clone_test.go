a319f263c914315a78fafd5a46cf164700abd6ed
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-10-26 11:16:40 -0700

    unit tests reflect clone pulls if-not-exists

--- engine/compiler/clone_test.go
+++ engine/compiler/clone_test.go
@@ -43,7 +43,7 @@ func TestClone(t *testing.T) {
 			ID:         "random",
 			Image:      "drone/git:latest",
 			Name:       "clone",
-			Pull:       engine.PullDefault,
+			Pull:       engine.PullIfNotExists,
 			RunPolicy:  engine.RunAlways,
 			WorkingDir: "/drone/src",
 			Volumes: []*engine.VolumeMount{
